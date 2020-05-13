44816628a27b10b4ad22176aa7ae50fdd6167e1b
Mohamed Desouky <mohamed.and9833@gmail.com>
2020-05-14 05:51:14 +0700

    finished the homePage and style the menu item component

---
+++ src/Components/Menu-item/Menu-item.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./Menu-item.style.scss";
+
+const Menu = ({ title, img, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${img}) `,
+      }}
+    />
+    <div className="cantainer">
+      <h3 className="title">{title.toUpperCase()}</h3>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+export default Menu;
---
+++ src/Pages/HomePage.Component/HomePage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./HomePage.style.scss";
+
+import Directory from "../../Components/Directory/Directory.component"
+
+const HomePage =()=>{
+return(
+    <div className="home-page">
+       <Directory />
+    </div>
+)
+}
+export default HomePage
